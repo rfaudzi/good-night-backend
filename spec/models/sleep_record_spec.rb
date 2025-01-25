0eb7d5b8b19cb0f74b9d317710680d0767548587
Rachman Faudzi <faudzirachman@gmail.com>
2025-01-25 19:10:57 +0700

    Clock-in: Fix SleepRecord model spec

--- spec/models/sleep_record_spec.rb
+++ spec/models/sleep_record_spec.rb
@@ -2,15 +2,14 @@ require 'rails_helper'
 
 RSpec.describe SleepRecord, type: :model do
   describe 'associations' do
-    it { should belong_to(:user) }
+    it { is_expected.to belong_to(:user) }
   end
 
   describe 'validations' do
-    it { should validate_presence_of(:start_time) }
-    it { should validate_presence_of(:end_time) }
+    it { is_expected.to validate_presence_of(:start_time) }
   end
 
-  describe '#end_time_after_start_time?' do
+  describe 'end_time validation' do
     let(:user) { create(:user) }
 
     context 'when end_time is before start_time' do
