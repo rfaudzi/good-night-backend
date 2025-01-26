2ee6dbac9ea8ea95c1877627862472f248a19125
Rachman Faudzi <faudzirachman@gmail.com>
2025-01-26 13:06:22 +0700

    fix rspec sleep record policy

--- spec/policies/sleep_record_policy_spec.rb
+++ spec/policies/sleep_record_policy_spec.rb
@@ -2,12 +2,25 @@ require 'rails_helper'
 require 'pundit/rspec'
 
 RSpec.describe SleepRecordPolicy, type: :policy do
-  let(:user) { User.new(id: 1) }
+  let(:user) { {user_id: 1, exp: Time.current.to_i + 1000} }
 
   subject { described_class }
 
+
   permissions :create? do
     it 'allows create sleep record' do
+      expect(subject).to permit(user)
+    end
+  end
+
+  permissions :update? do
+    it 'allows update sleep record' do
+      expect(subject).to permit(user, SleepRecord.new(user_id: user[:user_id]))
+    end
+  end
+
+  permissions :list? do
+    it 'allows list sleep record' do
       expect(subject).to permit(user)
     end
   end
