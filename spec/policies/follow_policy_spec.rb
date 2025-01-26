18fb96fe3041bc568f9ebb4db75dd27dd08d941b
Rachman Faudzi <faudzirachman@gmail.com>
2025-01-26 12:57:40 +0700

    Follow feature: Add follow policy for create

---
+++ spec/policies/follow_policy_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+require 'pundit/rspec'
+
+RSpec.describe FollowPolicy, type: :policy do
+  let(:user) { User.new }
+
+  subject { described_class }
+
+  permissions :create? do
+    it "allows a user to create a follow" do
+      expect(subject).to permit(user, Follow.new)
+    end
+  end
+end
+  
